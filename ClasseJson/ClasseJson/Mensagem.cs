17f2c793be8ecf849fa58319b7f5c0c35c1b131b
Marcelo Baptista <marcelo,dbaptista2@gmail.com>
2018-03-20 23:28:01 -0300

    Adicionando Arquivos

---
+++ ClasseJson/ClasseJson/Mensagem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasseJson
+{
+    public enum Mensagem
+    {
+        [CustomAttribute("1","Deu bom porra")]
+        item1 = 1,
+        [CustomAttribute("2", "Deu bom porra filho de uma puta")]
+        item2 = 2
+    }
+
+    public class CustomAttribute : Attribute
+    {
+        public CustomAttribute(string id, string mensagem)
+        {
+            this.Id = id;
+            Mensagem = mensagem;
+        }
+
+        public string Id { get; set; }
+        public string Mensagem { get; set; }
+    }
+
+  
+
+    public class MensagemJson
+    {
+        public string Id { get; set; }
+        public string Chave { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ ClasseJson/ClasseJson/Program.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClasseJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine(ToJson());
+
+            var t = JsonConvert.DeserializeObject<List<MensagemJson>>(ToJson());
+            Console.ReadKey();
+        }
+
+        public static string ToJson()
+        {
+            Array values = Enum.GetValues(typeof(Mensagem));
+            List<MensagemJson> jsonCompleto = new List<MensagemJson>();
+
+            foreach (Mensagem value in values)
+            {
+                MensagemJson json = new MensagemJson();
+
+                json.Chave = Enum.GetName(typeof(Mensagem), value);
+                json.Id = value.GetAttribute<CustomAttribute>().Id;
+                //json.Mensagem = value.GetAttribute<CustomAttribute>().Mensagem;
+                json.Mensagem = value.GetMensagem();
+             
+                jsonCompleto.Add(json);
+            }
+
+            return JsonConvert.SerializeObject(jsonCompleto);        
+        }      
+    }
+
+    public static class EnumExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+            return type.GetField(name) // I prefer to get attributes this way
+                .GetCustomAttributes(false)
+                .OfType<TAttribute>()
+                .SingleOrDefault();
+        }
+
+        public static string GetMensagem(this Mensagem value)            
+        {
+            var name = Enum.GetName(value.GetType(), value);
+
+            var attribute =
+           (CustomAttribute)
+           typeof(Mensagem)
+              .GetField(name)
+              .GetCustomAttributes(typeof(CustomAttribute), false).First();
+
+            return attribute.Mensagem;
+          
+        }
+    }
+}
